1d8d792e6600e3a057861a09b4811885fb661bc1
Antony74 <akb@akb.me.uk>
2025-04-06 19:48:19 +0100

    Got prettier working and started to center things

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
-import { Graph, InternalEvent } from '@maxgraph/core';
+import {
+    EdgeParameters,
+    Graph,
+    InternalEvent,
+    VertexParameters,
+} from '@maxgraph/core';
 
 const container = document.getElementById('graph-container');
 
@@ -12,52 +17,94 @@ const height = 500;
 container.style.width = `${width}`;
 container.style.height = `${height}`;
 
+const yCenter = width / 2;
+const xCenter = height / 2;
+const yOffset = 10;
+const top = 10;
+const bottom = 300;
+const left = 10;
+const right = 300;
+
 InternalEvent.disableContextMenu(container);
 
 const graph = new Graph(container);
 
 const parent = graph.getDefaultParent();
 
+const centerCoords = ({
+    position,
+    ...params
+}: VertexParameters): VertexParameters => {
+    if (position) {
+        return { ...params, position: [position[0], position[1]] };
+    } else {
+        return { ...params };
+    }
+};
+
 graph.batchUpdate(() => {
     graph.cellsLocked = true;
     graph.cellsSelectable = false;
 
-    const a = graph.insertVertex({
+    const vertexCommon: VertexParameters = {
         parent,
-        position: [10, 10],
         size: [100, 100],
+        style: { verticalAlign: 'middle' },
+    };
+
+    const a = graph.insertVertex({
+        ...vertexCommon,
+        position: [xCenter, top],
         value: 'a',
     });
 
     const b = graph.insertVertex({
-        parent,
-        position: [10, 10],
-        size: [100, 100],
+        ...vertexCommon,
+        position: [left, yCenter],
         value: 'b',
     });
 
     const c = graph.insertVertex({
-        parent,
-        position: [10, 10],
-        size: [100, 100],
+        ...vertexCommon,
+        position: [right, yCenter],
         value: 'c',
     });
 
     const d = graph.insertVertex({
-        parent,
-        position: [10, 10],
-        size: [100, 100],
+        ...vertexCommon,
+        position: [xCenter, bottom],
         value: 'd',
     });
 
-    graph.insertEdge({
+    const edgeCommon: EdgeParameters = {
         parent,
-        source: a,
-        target: b,
-        value: 'edge',
         style: {
             edgeStyle: 'orthogonalEdgeStyle',
             rounded: true,
         },
+    };
+
+    graph.insertEdge({
+        ...edgeCommon,
+        source: a,
+        target: b,
+    });
+
+    graph.insertEdge({
+        ...edgeCommon,
+        source: a,
+        target: c,
+    });
+
+    graph.insertEdge({
+        ...edgeCommon,
+        source: b,
+        target: d,
+    });
+
+    graph.insertEdge({
+        ...edgeCommon,
+        source: c,
+        target: d,
     });
 });
