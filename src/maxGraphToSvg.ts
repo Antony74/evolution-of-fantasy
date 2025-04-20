65810ee1023344dcc28bf945e6553d7e9af6b238
Antony74 <akb@akb.me.uk>
2025-04-20 14:22:19 +0100

    refactor: inlineImage deserves its own module

--- src/maxGraphToSvg.ts
+++ src/maxGraphToSvg.ts
@@ -1,7 +1,5 @@
-import fsp from 'fs/promises';
-
 import { Graph } from '@maxgraph/core';
-import mime from 'mime-types';
+import { inlineImage } from './inlineImage';
 
 export type MaxGraphToSvgOptions = {
     inlineImages?: boolean;
@@ -11,41 +9,18 @@ export const maxGraphToSvg = async (
     graph: Graph,
     options?: MaxGraphToSvgOptions,
 ): Promise<string> => {
-    const inlineImages: boolean =
-        (options ?? { inlineImages: false }).inlineImages ?? false;
-
     const container = graph.container;
 
     const orig = container.innerHTML;
+
+    const inlineImages: boolean =
+        (options ?? { inlineImages: false }).inlineImages ?? false;
 
     if (inlineImages) {
         const images = Array.from(container.querySelectorAll('image'));
 
         for (const image of images) {
-            let path = image.getAttribute('xlink:href') ?? '';
-
-            let isFile = false;
-
-            try {
-                const url = new URL(path);
-                isFile = url.protocol === 'file:';
-            } catch (_e) {}
-
-            if (isFile) {
-                path = path.slice(7);
-            } else {
-                throw new Error(`Can't inline "${path}".  Currently only file:// urls are supported.`)
-            }
-
-            const buffer = await fsp.readFile(path);
-            const content = buffer.toString('base64');
-
-            const mimeType = mime.lookup(path);
-            const contentType = mimeType ? mimeType : '';
-
-            const newUrl = `data:${contentType};base64, ${content}`;
-
-            image.setAttribute('xlink:href', newUrl);
+            await inlineImage(image);
         }
     }
 
