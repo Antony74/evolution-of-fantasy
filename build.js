1bde9998aa04ed39d1c6af9daf5a4c12b2fb8e77
Antony74 <akb@akb.me.uk>
2025-04-21 20:19:20 +0100

    refactor

--- build.js
+++ build.js
@@ -2,7 +2,6 @@ require('esbuild').build({
     entryPoints: ['src/index.ts'],
     bundle: true,
     outdir: 'dist',
-    treeShaking: true,
     platform: 'node',
     external: [
         'canvas',
