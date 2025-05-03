819d51ae631535c303c5914913b2230a4f8eeea5
Antony74 <akb@akb.me.uk>
2025-05-03 22:51:50 +0100

    GraphViz style.  Why does Hierarchical layout break it?

--- src/evolutionOfFantasy.ts
+++ src/evolutionOfFantasy.ts
@@ -1,4 +1,9 @@
-import { Cell, Graph, HierarchicalLayout } from 'maxgraph-core-commonjs';
+import {
+    Cell,
+    Graph,
+    HierarchicalLayout,
+    RadialTreeLayout,
+} from 'maxgraph-core-commonjs';
 
 import { Vec2 } from './utils';
 
@@ -13,11 +18,16 @@ export const createEvolutionOfFantasyGraph = (
 
     const graph = new Graph(container);
 
-    const size: Vec2 = [90, 70];
+    const size: Vec2 = [90, 90];
 
     graph.batchUpdate(() => {
-        const insertVertex = (value: string) => {
-            return graph.insertVertex({ value, size });
+        const insertVertex = (value: string, x: number, y: number) => {
+            const vertex = graph.insertVertex({
+                value,
+                size,
+                position: [x, y],
+            });
+            return vertex;
         };
 
         const insertEdge = (source: Cell, target: Cell) => {
@@ -26,28 +36,34 @@ export const createEvolutionOfFantasyGraph = (
 
         const vertexStyle = graph.getStylesheet().getDefaultVertexStyle();
         vertexStyle.shape = 'ellipse';
+        vertexStyle.perimeter = 'ellipsePerimeter';
         vertexStyle.strokeColor = 'black';
         vertexStyle.fillColor = 'none';
         vertexStyle.fontColor = 'black';
 
         const edgeStyle = graph.getStylesheet().getDefaultEdgeStyle();
         edgeStyle.strokeColor = 'black';
+        edgeStyle.align = undefined;
+        edgeStyle.fontColor = undefined;
+        edgeStyle.verticalAlign = undefined;
 
         console.log(JSON.stringify({ vertexStyle, edgeStyle }, null, 4));
 
-        const a = insertVertex(`Lord Dunsany`);
-        const b = insertVertex(`JRR Tolkien`);
-        const c = insertVertex(`Robert E Howard`);
-        const d = insertVertex(`Glen Cook`);
+        const a = insertVertex(`Lord Dunsany`, 60, 0);
+        const b = insertVertex(`JRR Tolkien`, 120, 190);
+        const c = insertVertex(`Robert E Howard`, 0, 190);
+        const d = insertVertex(`Glen Cook`, 60, 380);
 
         insertEdge(a, b);
         insertEdge(a, c);
         insertEdge(b, d);
         insertEdge(c, d);
-    });
 
-    const layout = new HierarchicalLayout(graph);
-    layout.execute(graph.getDefaultParent());
+        // const layout = new HierarchicalLayout(graph);
+        // layout.execute(graph.getDefaultParent());
+
+        // vertices.forEach(vertex => console.log(vertex.geometry?.getPoint()))
+    });
 
     return graph;
 };
